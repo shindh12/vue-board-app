3af5eee8ee47408711f696796b4b7844023fdc5a
donghashin <shindh159@gmail.com>
2018-09-14 23:02:35 +0900

    초기 프로젝트 생성

---
+++ portal-rest-server/src/main/java/com/portal/demo/PortalRestServerApplication.java
@@ -0,0 +1,12 @@
+package com.portal.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortalRestServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortalRestServerApplication.class, args);
+	}
+}
